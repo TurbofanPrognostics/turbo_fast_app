5350bdbafd3b712c88837a2abac412a30fda6ad5
Leon Adams <skorpio11@gmail.com>
2021-08-24 12:06:14 -0400

    Change localhost to 0.0.0.0 for standard deployment.

--- application.py
+++ application.py
@@ -44,13 +44,13 @@ def predict_rul(turbofan: Models.TurboFan):
     )
     return {"prediction": prediction}
 
-# Index route, opens automatically on http://127.0.0.1:8000
+# Index route, opens automatically on http://0.0.0.0:8000
 @app.get("/")
 def index():
     return {"message": "Welcome Turbofan Prognostics!"}
 
 # Run the API with uvicorn
-# Will run on http://127.0.0.1:8000
+# Will run on http://0.0.0.0:8000
 
 if __name__ == "__main__":
     uvicorn.run("application:app", host="0.0.0.0", port=8000)
